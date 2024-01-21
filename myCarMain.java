ffa081a35c86d763c5265c05dda66f0cec633196
배주연 <99193199+juyeon-Bae@users.noreply.github.com>
2024-01-21 15:53:30 +0900

    객체 생성 시 생성자 선택

---
+++ myCarMain.java
@@ -0,0 +1,29 @@
+
+public class myCarMain {
+
+	public static void main(String[] args) {
+		 myCar car1 = new myCar(); //생성자 선택
+		 System.out.println("car1.company : "+car1.company);
+		 System.out.println();
+		 
+		 myCar car2 = new myCar("자가용"); 
+			 System.out.println("car2.company : "+car2.company);
+			 System.out.println("car2.model : "+car2.model);
+			 System.out.println();
+			 
+		 myCar car3 = new myCar("자가용","빨강"); 
+		 System.out.println("car3.company : "+car3.company);
+		 System.out.println("car3.model : "+car2.model);
+		 System.out.println("car3.color : "+car3.color);
+		 System.out.println();
+		 
+		 myCar car4 = new myCar("택시","검정",200);
+		 System.out.println("car4.company : "+ car4.company);
+		 System.out.println("car4.model : "+car4.model);
+		 System.out.println("car4.color : "+car4.color);
+		 System.out.println("car4.maxSpeed : "+car4.maxSpeed);
+		  
+		 
+
+}
+}
