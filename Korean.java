db458ef8d7641bbe07c5ff8ea70d9f654c19daa9
배주연 <99193199+juyeon-Bae@users.noreply.github.com>
2024-01-21 15:38:50 +0900

    생성자에서 필드 초기화
    매개 변수

---
+++ Korean.java
@@ -0,0 +1,11 @@
+
+public class Korean {
+	String name;
+	String ssn;
+	
+	//생성자에서 필드 초기화
+	public Korean(String name, String ssn ) { //매개 변수 : 외부 값을 생성자 내부 블록으로 들이는 것
+		this.name = name;
+		this.ssn = ssn;
+	}
+}
