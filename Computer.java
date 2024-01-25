9395150fd8245bdd305f4826840e789cad472ca3
배주연 <99193199+juyeon-Bae@users.noreply.github.com>
2024-01-25 20:00:45 +0900

    ✨ 자식클래스 생성(1-2)
    매소드 오버라이딩

---
+++ Computer.java
@@ -0,0 +1,8 @@
+package sec03.exam1;
+
+public class Computer extends Calculator {
+		double areaCircle(double r) {
+			System.out.println("Computer 객체의 areaCircle()실행");
+			return Math.PI*r*r;
+		}
+}
