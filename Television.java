02a6bc09c1f87a39f123fcf91e5ded1e61d754ac
배주연 <99193199+juyeon-Bae@users.noreply.github.com>
2024-01-29 19:56:23 +0900

    ✨ Television 인터페이스 구현

---
+++ Television.java
@@ -0,0 +1,27 @@
+package sec07.exam01;
+
+public class Television implements RemoteControl{ //인터페이스 생성
+	//상수
+	private int volume;
+	
+	//추상 메소드 오버라이드
+	public void turnOn() {
+		System.out.println("TV를 켭니다.");
+	}
+	
+	public void turnOff() {
+		System.out.println("TV를 끕니다.");
+	}
+	
+	public void setVolume(int volume) {
+		if(volume>RemoteControl.MAX_VOLUME) {
+			this.volume = RemoteControl.MAX_VOLUME;
+		}
+		else if(volume<RemoteControl.MIN_VOLUME) {
+			this.volume = RemoteControl.MIN_VOLUME;
+		}else {
+			this.volume = volume;
+		}
+		System.out.println("현재 TV 볼륨 : "+this.volume);
+	}
+}
