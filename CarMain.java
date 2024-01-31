87761a61d6c0020156a8edbf1c548f80507161ec
배주연 <99193199+juyeon-Bae@users.noreply.github.com>
2024-01-31 16:16:43 +0900

    ✨필드 다형성 테스트

--- CarMain.java
+++ CarMain.java
@@ -1,14 +1,16 @@
+package sec08.exam;
 
 public class CarMain {
 
 	public static void main(String[] args) {
-		Car myCar = new Car(); //객체 생성
-		myCar.maxSpeed = 60; //필드 값 변경
-		System.out.println(myCar.company);
-		System.out.println(myCar.model);
-		System.out.println(myCar.speed); //0
-		System.out.println(myCar.color);
-		System.out.println(myCar.maxSpeed);
+		Car mycar = new Car();
+		
+		mycar.run(); //HkTire
+		
+		mycar.frontLeftTire = new GhTire();
+		mycar.frontRightTire = new GhTire();
+		
+		mycar.run();
 	}
 
 }
