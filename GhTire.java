e10f390e6220c4415146b1c63229a4989bc10f3d
배주연 <99193199+juyeon-Bae@users.noreply.github.com>
2024-01-31 16:15:58 +0900

    ✨ 금호, 한국 타이어 구현클래스

---
+++ GhTire.java
@@ -0,0 +1,8 @@
+package sec08.exam;
+
+public class GhTire implements Tire {
+	public void roll() {
+		//Tire 인터페이스 구현
+		System.out.println("금호 타이어가 굴러갑니다."); 
+	}
+}
---
+++ HkTire.java
@@ -0,0 +1,8 @@
+package sec08.exam;
+
+public class HkTire implements Tire {
+	public void roll() {
+		//Tire 인터페이스 구현
+		System.out.println("한국 타이어가 굴러갑니다."); 
+	}
+}
